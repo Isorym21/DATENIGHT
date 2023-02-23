f3d8ae7f7117d26508a2928326d4b0cfbec7ebe1
GSchrils <gschrils@protonmail.com>
2023-02-23 12:00:49 -0500

    Added div classnames and css styling
    moved the list into a table so it displays horizontally

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -4,17 +4,19 @@ import "./Footer.css"; // Importing the Style of the footer
 
 export function Footer() {
   return (
-    <div>
+    <div className="footerall">
     <span onClick={() => window.scroll(0, 0)} className="footer">
     🌆 Date Night 💃🕺 Brought to you by:
     </span>
-    <ul className="authorgitlinks">
-    <li><Link to='https://github.com/BryCor'>Bryan Cortez</Link></li>
-    <li><Link to='https://github.com/WillsPrettyLxzy'>Will Estrada</Link></li>
-    <li><Link to='https://github.com/gerardom41'>Gerardo Molina</Link></li>
-    <li><Link to='https://github.com/Isorym21'>Isory Santana</Link></li>
-    <li><Link to='https://github.com/GSchrils'>George Schrils</Link></li>
-    </ul>
+      <div className="table">
+        <ul className="authorgitlinks">
+          <li><Link to='https://github.com/BryCor'>Bryan Cortez</Link></li>
+          <li><Link to='https://github.com/WillsPrettyLxzy'>Will Estrada</Link></li>
+          <li><Link to='https://github.com/gerardom41'>Gerardo Molina</Link></li>
+          <li><Link to='https://github.com/Isorym21'>Isory Santana</Link></li>
+          <li><Link to='https://github.com/GSchrils'>George Schrils</Link></li>
+        </ul>
+      </div>
     </div>
   );
 }
