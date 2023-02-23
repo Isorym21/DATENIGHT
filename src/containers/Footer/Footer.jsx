9c2350261351b47392cd22d17d62b21b6ddc9e76
GSchrils <gschrils@protonmail.com>
2023-02-23 10:43:27 -0500

    added a footer css and links to all of our githubs

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import "./Footer.css"; // Importing the Style of the footer 
 
 export function Footer() {
   return (
     <div>
-      <h1>Footer</h1>
+    <span onClick={() => window.scroll(0, 0)} className="footer">
+      Project: 🌆 Date Night 💃🕺 Brought to you by:
+    </span>
+    <h1></h1><Link to='https://github.com/GSchrils'>George Schrils</Link>
+    <h1></h1><Link to='https://github.com/Isorym21'>Isory Santana</Link>
+    <h1></h1><Link to='https://github.com/BryCor'>Bryan Cortez</Link>
+    <h1></h1><Link to='https://github.com/gerardom41'>Gerardo Molina</Link>
+    <h1></h1><Link to='https://github.com/WillsPrettyLxzy'>Will Estrada</Link>
+    
     </div>
   );
 }
