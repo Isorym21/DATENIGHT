02bf76d5f6663b7dea877854576aa4a122dd5156
GSchrils <gschrils@protonmail.com>
2023-02-23 10:48:52 -0500

    Reorganized the links alphabetically by last name.

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -8,12 +8,11 @@ export function Footer() {
     <span onClick={() => window.scroll(0, 0)} className="footer">
       Project: 🌆 Date Night 💃🕺 Brought to you by:
     </span>
-    <h1></h1><Link to='https://github.com/GSchrils'>George Schrils</Link>
-    <h1></h1><Link to='https://github.com/Isorym21'>Isory Santana</Link>
     <h1></h1><Link to='https://github.com/BryCor'>Bryan Cortez</Link>
-    <h1></h1><Link to='https://github.com/gerardom41'>Gerardo Molina</Link>
     <h1></h1><Link to='https://github.com/WillsPrettyLxzy'>Will Estrada</Link>
-    
+    <h1></h1><Link to='https://github.com/gerardom41'>Gerardo Molina</Link>
+    <h1></h1><Link to='https://github.com/Isorym21'>Isory Santana</Link>
+    <h1></h1><Link to='https://github.com/GSchrils'>George Schrils</Link>
     </div>
   );
 }
